c361c210f5f796c96c48fa3e6493e20e6d67e5fe
Douglas Caetano dos Santos <douglas@douglascaetano.com>
2019-01-09 20:34:53 -0200

    2018/02: second part
    The code for this part is terrible. It really needs more love.
    
    Both the function to calculate the Hamming distance and the function
    to get the common letters of a string need a more idiomatic code.
    Certainly there is better ways to express them in a more rustic way.
    
    Besides that, I found out that this string comparison is called
    Hamming distance and found about the zip() function. :)

--- 2018/02/src/main.rs
+++ 2018/02/src/main.rs
@@ -1,6 +1,7 @@
 use std::io::{self, Read};
 use std::process;
 use std::collections::HashMap;
+use std::cmp;
 
 fn main() {
     println!("Advent of Code 2018 - Day 2");
@@ -17,10 +18,12 @@ fn main() {
     println!("Your input has {} entr{}.", ilen, if ilen == 1 { "y" } else { "ies" });
     println!();
 
+    let ids: Vec<_> = input.lines().collect();
+
     let mut two_rep_cnt = 0;
     let mut three_rep_cnt = 0;
 
-    for id in input.lines() {
+    for id in ids.iter() {
         let (two_rep, three_rep) = count_repetition(&id);
         if two_rep { two_rep_cnt += 1; }
         if three_rep { three_rep_cnt += 1; }
@@ -29,6 +32,18 @@ fn main() {
     let checksum = two_rep_cnt * three_rep_cnt;
 
     println!("The checksum for the provided IDs is: {}", checksum);
+
+    'outer: for i in 0..ids.len() {
+        for j in i..ids.len() {
+            if hamming_distance(&ids[i], &ids[j]) == 1 {
+                println!("Found two IDs whose difference is one char:");
+                println!("- {}\n- {}", ids[i], ids[j]);
+                println!();
+                println!("The common letters between the IDs are: {}", common_letters(&ids[i], &ids[j]));
+                break 'outer;
+            }
+        }
+    }
 }
 
 fn count_repetition(id: &str) -> (bool, bool) {
@@ -53,6 +68,36 @@ fn count_repetition(id: &str) -> (bool, bool) {
     (two_rep, three_rep)
 }
 
+fn hamming_distance(a: &str, b: &str) -> u32 {
+    if a.len() != b.len() {
+        panic!("String lengths differ, they must be the same.");
+    }
+
+    let mut sum = 0;
+
+    for pair in a.chars().zip(b.chars()) {
+        if pair.0 != pair.1 {
+            sum += 1;
+        }
+    }
+
+    sum
+}
+
+fn common_letters(a: &str, b: &str) -> String {
+    let mut r = String::new();
+
+    let m = cmp::min(a.len(), b.len());
+    for i in 0..m {
+        // WARNING: UGLY, UGLY CODE BELOW!
+        if a[i..].chars().next().unwrap() == b[i..].chars().next().unwrap() {
+            r.push(a[i..].chars().next().unwrap());
+        }
+    }
+
+    r
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -71,6 +116,38 @@ mod tests {
 
         for (id, rep) in ids {
             assert_eq!(rep, count_repetition(&id));
+        }
+    }
+
+    #[test]
+    fn check_hamming_distance() {
+        let string_pairs = vec![
+            (("abcdef", "abcdef"), 0),
+            (("abcdef", "abcddd"), 2),
+            (("abcdef", "fedcba"), 6)
+        ];
+
+        for (pair, result) in string_pairs {
+            assert_eq!(result, hamming_distance(&pair.0, &pair.1));
+        }
+    }
+
+    #[test]
+    #[should_panic]
+    fn check_hamming_panic() {
+        let _ = hamming_distance("big string", "small string");
+    }
+
+    #[test]
+    fn check_common_letters() {
+        let string_pairs = vec![
+            (("abcdef", "abcdef"), "abcdef"),
+            (("abcdef", "abceef"), "abcef"),
+            (("abcbab", "abcaba"), "abc")
+        ];
+
+        for (pair, result) in string_pairs {
+            assert_eq!(result, common_letters(&pair.0, &pair.1));
         }
     }
 }
