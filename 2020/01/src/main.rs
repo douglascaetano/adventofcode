809112e26614b234092d1d5906a3835262045a02
Douglas Caetano dos Santos <douglas@douglascaetano.com>
2020-12-06 19:25:57 -0300

    2020/01: add a bit of structure
    Extract some code into functions on their own.

--- 2020/01/src/main.rs
+++ 2020/01/src/main.rs
@@ -6,25 +6,31 @@ use itertools::Itertools;
 fn main() {
     println!("Advent of Code 2020 - Day 1");
 
-    let entries: Vec<u32> = {
-        let mut s = String::new();
-        io::stdin()
-            .read_to_string(&mut s)
-            .expect("only valid UTF-8 input");
-        s.lines()
-            .map(|s| s.parse().expect("only valid integers"))
-            .collect()
-    };
+    let entries = get_entries();
 
     println!("Total entries: {}", entries.len());
 
-    let pair = entries
-        .into_iter()
-        .combinations(2)
-        .find(|v| v[0] + v[1] == 2020)
-        .expect("input containing a pair of values that sum 2020");
+    let pair = find_pair(entries);
 
     println!("Found pair:");
     println!("  {} + {} = {}", pair[0], pair[1], pair[0] + pair[1]);
     println!("  {} * {} = {}", pair[0], pair[1], pair[0] * pair[1]);
+}
+
+fn get_entries() -> Vec<u32> {
+    let mut s = String::new();
+    io::stdin()
+        .read_to_string(&mut s)
+        .expect("only valid UTF-8 input");
+    s.lines()
+        .map(|s| s.parse().expect("only valid integers"))
+        .collect()
+}
+
+fn find_pair(entries: Vec<u32>) -> Vec<u32> {
+    entries
+        .into_iter()
+        .combinations(2)
+        .find(|v| v[0] + v[1] == 2020)
+        .expect("input containing a pair of values that sum 2020")
 }
