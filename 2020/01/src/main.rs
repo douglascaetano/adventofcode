059905175ff721e117bcd7702e8036ac8326f18b
Douglas Caetano dos Santos <douglas@douglascaetano.com>
2020-12-06 20:03:13 -0300

    2020/01: part two of this day, lots of changes
    Added StructOpt so one can set the criteria they want. It's possible to
    define the size of combination and the value of the sum.
    
    Change find_pair() to find_combination() for obvious reasons.
    
    Also, of course, there was a better way to access the elements and find
    sum and product.

--- 2020/01/src/main.rs
+++ 2020/01/src/main.rs
@@ -2,21 +2,41 @@ use std::io;
 use std::io::Read;
 
 use itertools::Itertools;
+use structopt::StructOpt;
+
+/// Reads entries from stdin and find a combination of given size that sums to the given value.
+#[derive(Debug, StructOpt)]
+#[structopt(name = "aoc2001")]
+struct Opt {
+    /// Size of the desired combination of entries to find the sum
+    #[structopt(short, long, default_value = "2")]
+    combination_size: usize,
+
+    /// Sum to be found in combinations
+    #[structopt(short, long, default_value = "2020")]
+    sum: u32,
+}
 
 fn main() {
+    let opt = Opt::from_args();
+
     println!("Advent of Code 2020 - Day 1");
 
     let entries = get_entries();
 
     println!("Total entries: {}", entries.len());
 
-    let pair = find_pair(entries).expect("input containing a pair of values that sum 2020");
-    let a = pair[0];
-    let b = pair[1];
+    let comb = match find_combination(opt.combination_size, opt.sum, entries) {
+        Some(comb) => comb,
+        None => {
+            println!("No combination of entries found that match the given criteria.");
+            std::process::exit(1);
+        }
+    };
 
-    println!("Found pair:");
-    println!("  {} + {} = {}", a, b, a + b);
-    println!("  {} * {} = {}", a, b, a * b);
+    println!("Found combination: {:?}", comb);
+    println!("      Sum: {}", comb.iter().sum::<u32>());
+    println!("  Product: {}", comb.iter().product::<u32>());
 }
 
 fn get_entries() -> Vec<u32> {
@@ -29,11 +49,11 @@ fn get_entries() -> Vec<u32> {
         .collect()
 }
 
-fn find_pair(entries: Vec<u32>) -> Option<Vec<u32>> {
+fn find_combination(size: usize, sum: u32, entries: Vec<u32>) -> Option<Vec<u32>> {
     entries
         .into_iter()
-        .combinations(2)
-        .find(|v| v[0] + v[1] == 2020)
+        .combinations(size)
+        .find(|v| sum == v.iter().sum())
 }
 
 #[cfg(test)]
@@ -42,14 +62,14 @@ mod tests {
 
     #[test]
     fn test_find_pair() {
-        let pair = find_pair(vec![2, 10, 1000, 9999, 1020]).expect("a pair");
+        let pair = find_combination(vec![2, 10, 1000, 9999, 1020]).expect("a pair");
         assert_eq!(pair, vec![1000, 1020]);
         assert_eq!(pair.len(), 2);
     }
 
     #[test]
     fn test_find_pair_fail() {
-        let pair = find_pair(vec![2, 10, 1000, 9999]);
+        let pair = find_combination(vec![2, 10, 1000, 9999]);
         assert!(pair.is_none());
     }
 }
