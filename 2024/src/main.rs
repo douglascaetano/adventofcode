86eac99f24fd2663eea947904aa36cc10cc32f02
Douglas C Santos <douglas@douglascaetano.com>
2024-12-02 21:04:42 -0300

    2024, day 1, part 2

--- 2024/src/main.rs
+++ 2024/src/main.rs
@@ -1,7 +1,8 @@
-fn day1() {
-    const INPUT: &str = include_str!("../input/day1_1.txt");
+const INPUT: &str = include_str!("../input/day1_1.txt");
+const SAMPLE: &str = include_str!("../input/day1_sample.txt");
 
-    let (mut list_a, mut list_b) = INPUT.lines().fold(
+fn day1(input: &str) {
+    let (mut list_a, mut list_b) = input.lines().fold(
         (Vec::new(), Vec::new()),
         |(mut list_a, mut list_b), line| {
             let mut values = line.split_whitespace();
@@ -19,15 +20,24 @@ fn day1() {
     list_b.sort();
 
     let sum = list_a
-        .into_iter()
-        .zip(list_b.into_iter())
+        .iter()
+        .zip(list_b.iter())
         .fold(0, |acc, (a, b)| acc + (a - b).abs());
 
     println!("Part 1: final sum is {}", sum);
+
+    let score = list_a
+        .iter()
+        .map(|&location_id| {
+            list_b.iter().filter(|&&id| id == location_id).count() as i32 * location_id
+        })
+        .sum::<i32>();
+
+    println!("Part 2: final score is {}", score);
 }
 
 fn main() {
     println!("Hello, Advent of Code 2024!");
 
-    day1();
+    day1(INPUT);
 }
